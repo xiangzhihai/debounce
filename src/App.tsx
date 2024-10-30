b0fb81fa7544a430bcc805661a844f79470be33d
Zhihai Xiang <16368357+xiangzhihai@users.noreply.github.com>
2024-10-29 19:05:48 -0700

    Wrote an basic debounce

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState<string>("");
+  const [debouncedInput, setDebouncedInput] = useState<string>("");
+  const timeOutRef = useRef<number | null>(null);
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInput(event.target.value);
+    if (timeOutRef.current) {
+      clearTimeout(timeOutRef.current);
+    }
+
+    timeOutRef.current = setTimeout(() => {
+      setDebouncedInput(event.target.value);
+    }, 2000);
+  }
+
+  useEffect(() => {
+    return (() => {
+      if (timeOutRef.current) {
+        clearTimeout(timeOutRef.current);
+      }
+    })
+  }, []);
+
+  return (
+    <>
+      <div className="card">
+        <span>Input with instant feedback: </span>
+        <input value={input} onChange={handleInputChange}/>
+        <p>
+          Here is a debonced input with 2 seconds: {debouncedInput}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
